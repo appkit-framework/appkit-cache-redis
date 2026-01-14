32bf6cc66a56ab4ffbe6bbadde2b6048afb15654
appkit-framework <admin@appkit.biz>
2026-01-14 19:18:24 +0100

    Initial commit

---
+++ src/RedisCache.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppKit\Cache\Redis;
+
+use AppKit\Health\HealthIndicatorInterface;
+use AppKit\Health\HealthCheckResult;
+use AppKit\Cache\CacheInterface;
+use AppKit\Json\Json;
+
+class RedisCache implements HealthIndicatorInterface, CacheInterface {
+    private $client;
+
+    function __construct($client) {
+        $this -> client = $client;
+    }
+
+    public function checkHealth() {
+        return new HealthCheckResult([
+            'Redis client' => $this -> client
+        ]);
+    }
+
+    public function has($key) {
+        return (bool) $this -> client -> exists($key);
+    }
+
+    public function get($key) {
+        $value = $this -> client -> get($key);
+        if($value === null)
+            return null;
+        return Json::decode($value);
+    }
+
+    public function set($key, $value, $ttl = 0) {
+        $value = Json::encode($value);
+        if(!$ttl)
+            $this -> client -> set($key, $value);
+        else
+            $this -> client -> set($key, $value, 'EX', $ttl);
+    }
+
+    public function increment($key, $by = 1) {
+        return $this -> client -> incrby($key, $by);
+    }
+
+    public function decrement($key, $by = 1) {
+        return $this -> client -> decrby($key, $by);
+    }
+
+    public function delete($key) {
+        return $this -> client -> delete($key);
+    }
+
+    public function clear() {
+        return $this -> client -> flushdb();
+    }
+}
